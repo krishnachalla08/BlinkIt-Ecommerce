13d51f42acaa912a778b58fde025218de2653317
sujith bhumarapu <sujithbhumarapu@gmail.com>
2026-03-17 23:12:46 +0530

    added s3 signed url

---
+++ Backend/product-service/src/main/java/com/blinkit/product_service/Enums/ContentType.java
@@ -0,0 +1,6 @@
+package com.blinkit.product_service.Enums;
+
+public enum ContentType {
+    IMAGE,
+    VIDEO
+}
---
+++ Backend/product-service/src/main/java/com/blinkit/product_service/S3Client/SignedUrl.java
@@ -0,0 +1,37 @@
+package com.blinkit.product_service.S3Client;
+
+import com.blinkit.product_service.Enums.ContentType;
+import com.blinkit.product_service.dto.S3UrlRequest;
+import com.blinkit.product_service.dto.S3UrlResponse;
+import com.blinkit.product_service.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@AllArgsConstructor
+public class SignedUrl {
+    private final S3ImageService s3ImageService;
+
+    @GetMapping("/s3/signed-url")
+    private Object s3Url(@RequestBody S3UrlRequest s3UrlRequest, HttpServletResponse response) {
+        try {
+            if (s3UrlRequest.getContentType().equals(ContentType.IMAGE.toString())) {
+                response.setStatus(200);
+                return s3ImageService.getSignedS3Url(s3UrlRequest.getFileName());
+            } else {
+                response.setStatus(400);
+                return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid Content Type", LocalDateTime.now());
+
+            }
+        }catch(Exception e){
+            response.setStatus(500);
+            return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), LocalDateTime.now());
+        }
+    }
+}
